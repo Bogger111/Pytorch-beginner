88fb530fd0535423e16d07cb14699e2129da826a
Bogger111 <cengyibo1@gmail.com>
2026-04-25 16:19:42 +0800

    new

--- 13_ResNet18.py
+++ 13_ResNet18.py
@@ -33,20 +33,31 @@ models = {}
 results = {}
 
 class ResidualBlock(nn.Module):
+    expansion = 1
+
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1, stride=stride)
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels,
+            kernel_size=3, stride=stride, padding=1, bias=False
+        )
         self.bn1 = nn.BatchNorm2d(out_channels)
 
-        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels,
+            kernel_size=3, stride=1, padding=1, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(out_channels)
 
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU(inplace=True)
 
-        if in_channels != out_channels or stride != 1:
+        if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, 1, stride=stride),
+                nn.Conv2d(
+                    in_channels, out_channels,
+                    kernel_size=1, stride=stride, bias=False
+                ),
                 nn.BatchNorm2d(out_channels)
             )
         else:
@@ -55,59 +66,63 @@ class ResidualBlock(nn.Module):
     def forward(self, x):
         identity = self.shortcut(x)
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
 
         out = out + identity
         out = self.relu(out)
 
         return out
-    
-class ResNet(nn.Module):
-    def __init__(self, use_dropout=False):
+
+class ResNet18(nn.Module):
+    def __init__(self, num_classes=10, use_dropout=False):
         super().__init__()
 
-        self.stem = nn.Sequential(
-            nn.Conv2d(3, 32, 3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU()
-        )
+        self.in_channels = 64
 
-        self.layer1 = ResidualBlock(32, 32, stride=1)
-        self.layer2 = ResidualBlock(32, 64, stride=2)
-        self.layer3 = ResidualBlock(64, 128, stride=2)
+        self.conv1 = nn.Conv2d(3, 64, 3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
 
-        self.pool = nn.MaxPool2d(2)
-        self.flat = nn.Flatten(start_dim=1)
+        self.layer1 = self._make_layer(64, 2, stride=1)
+        self.layer2 = self._make_layer(128, 2, stride=2)
+        self.layer3 = self._make_layer(256, 2, stride=2)
+        self.layer4 = self._make_layer(512, 2, stride=2)
 
-        self.fc = nn.Sequential(
-            nn.Linear(128 * 4 * 4, 128),
-            nn.ReLU()
-        )
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
         if use_dropout:
             self.dropout = nn.Dropout(0.3)
         else:
             self.dropout = nn.Identity()
 
-        self.out = nn.Linear(128, 10)
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, out_channels, blocks, stride):
+        layers = []
+
+        layers.append(ResidualBlock(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels
+
+        for _ in range(1, blocks):
+            layers.append(ResidualBlock(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.stem(x)      
-        x = self.pool(x)      
+        x = self.relu(self.bn1(self.conv1(x)))
 
-        x = self.layer1(x)    
-        x = self.layer2(x)    
-        x = self.layer3(x)    
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
-        x = self.flat(x)
-        x = self.fc(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
         x = self.dropout(x)
-        x = self.out(x)
+        x = self.fc(x)
+
         return x
 
 def model_train(model,data_loader,optimizer,criterion):
@@ -165,7 +180,7 @@ def model_running(configs):
         print(name)
 
         torch.manual_seed(24)
-        model = ResNet(use_dropout=cfg['dropout']).to(device)
+        model = ResNet18(use_dropout=cfg['dropout']).to(device)
         models[name] = model
         
         optimizer = optim.Adam(model.parameters(),lr=cfg['lr'],weight_decay=1e-4)
